16aa971efc7fe4308e0addc0f8cee2186e7b0130
Lucas Walger do Nascimento <walger.profissional@gmail.com>
2022-05-30 23:13:34 -0300

    Atualizacoes na cena Menu

--- Cenas/Menu.cpp
+++ Cenas/Menu.cpp
@@ -8,30 +8,4 @@ namespace Cenas
     {
         *this+= new EntidadeMenuPrincipal();
     }
-    void Menu::novoJogo()
-    {
-        
-        cout<<"new Game\n";
-        MudarCena(0);
-    }
-    void Menu::CenaUm()
-    {}
-    void Menu::CenaDois()
-    {}
-    void Menu::UmJogador()
-    {}
-    void Menu::DoisJogadores()
-    {}
-    void Menu::carregarJogoAntigo()
-    {
-        cout<<"Jogo Antigo\n";
-    }
-    void Menu::leaderBoard()
-    {}
-    void Menu::sair()
-    {
-        GerenciadorInputs::FinalizarJogo();
-    }
-    void Menu::voltar()
-    {}
 }
--- Cenas/Menu.h
+++ Cenas/Menu.h
@@ -5,15 +5,6 @@ namespace Cenas
     class Menu: public Cena
     {
         private:
-            void novoJogo();
-            void CenaUm();
-            void CenaDois();
-            void UmJogador();
-            void DoisJogadores();
-            void carregarJogoAntigo();
-            void leaderBoard();
-            void sair();
-            void voltar();
             void Carregar();
         public:
             Menu(GerenciadorCenas* ger=nullptr)
